f51bced220538d1a3742cc3e52ecceb094cae79b
UbbathotiNagamalleswari <135152936+UbbathotiNagamalleswari@users.noreply.github.com>
2023-08-31 22:29:14 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,7 +14,6 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
@@ -26,8 +24,7 @@ $(document).ready(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
-
-    // toggle menu/navbar script
+    
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
@@ -47,8 +44,7 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-
-    // owl carousel script
+    
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
